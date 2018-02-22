61956147a036a2c896d062ef5948d8d8d57ac683
Marcel Roesink <RSK01@rocvantwente.nl>
2018-02-22 21:06:29 +0100

    Start voor bijles

---
+++ Bijles/Password.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bijles
+{
+    public class Password
+    {
+        private string password = "";
+        private Random random = new Random();
+
+        /// <summary>
+        /// Generate a random password
+        /// </summary>
+        /// <param name="capitals"></param>
+        /// <param name="noncapitals"></param>
+        /// <param name="digits"></param>
+        /// <param name="other"></param>
+        /// <returns></returns>
+        public string CreateRandomPassword(
+            int capitals, int noncapitals, int digits, int other)
+        {
+            if (digits == 0 || capitals == 0 || noncapitals == 0 ||
+                other == 0)
+            {
+                throw new Exception(
+                    "The password must have at least one character per category!");
+            }
+            if (digits + capitals + noncapitals + other < 6)
+            {
+                throw new Exception(
+                    "The password must have at least six characters!");
+            }
+            // Add digits to the password
+            AddDigits(digits);
+
+            return password;
+        }
+
+        /// <summary>
+        /// Add [numberOfDigits] digits to the password
+        /// </summary>
+        /// <param name="numberOfDigits"></param>
+        private void AddDigits(int numberOfDigits)
+        {
+            for (int i = 0; i < numberOfDigits; i++)
+            {
+                int randomnumber = random.Next(0, 10);
+                password += randomnumber.ToString();
+            }
+        }
+    }
+}
--- Bijles/Program.cs
+++ Bijles/Program.cs
@@ -10,6 +10,51 @@ namespace Bijles
     {
         static void Main(string[] args)
         {
+            int cijfers , hoofdletters , kleineletters , speciaal = 0;
+            
+            Console.Write("Hoeveel cijfers wilt u in het wachtwoord? ");
+            while(!int.TryParse(Console.ReadLine() , out cijfers))
+            {
+                ShowError("Hoeveel cijfers wilt u in het wachtwoord? ");
+            }
+
+            Console.Write("Hoeveel hoofdletters wilt u in het wachtwoord? ");
+            while (!int.TryParse(Console.ReadLine(), out hoofdletters))
+            {
+                ShowError("Hoeveel hoofdletters wilt u in het wachtwoord? ");
+            }
+
+            Console.Write("Hoeveel kleine letters wilt u in het wachtwoord? ");
+            while (!int.TryParse(Console.ReadLine(), out kleineletters))
+            {
+                ShowError("Hoeveel kleine letters wilt u in het wachtwoord? ");
+            }
+
+            Console.Write("Hoeveel speciale tekens wilt u in het wachtwoord? ");
+            while (!int.TryParse(Console.ReadLine(), out speciaal))
+            {
+                ShowError("Hoeveel speciale tekens wilt u in het wachtwoord? ");
+            }
+
+            Password pw = new Password();
+            string randomPassword = pw.CreateRandomPassword(hoofdletters, kleineletters,
+                cijfers, speciaal);
+
+            Console.WriteLine("Het gegenereerde wachtwoord is {0}",
+                randomPassword);
+            Console.ReadKey();
+        }
+
+        private static void ShowError(string message)
+        {
+            ConsoleColor oldBackgroundColor = Console.BackgroundColor;
+            ConsoleColor oldForegroundColor = Console.ForegroundColor;
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Onjuiste invoer");
+            Console.WriteLine(message);
+            Console.BackgroundColor = oldBackgroundColor;
+            Console.ForegroundColor = oldForegroundColor;
         }
     }
 }
